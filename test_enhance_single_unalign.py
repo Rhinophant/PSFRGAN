76bfb8629e972a1722e42435eb94d43e181e58d5
error12368 <sb782396771@163.com>
2022-06-17 16:11:43 +0800

    update

--- test_enhance_single_unalign.py
+++ test_enhance_single_unalign.py
@@ -4,7 +4,7 @@ This script enhance all faces in one image with PSFR-GAN and paste it back to th
 import dlib
 import os
 import cv2
-import numpy as np 
+import numpy as np
 from tqdm import tqdm
 from skimage import transform as trans
 from skimage import io
@@ -18,7 +18,7 @@ from models import create_model
 def detect_and_align_faces(img, face_detector, lmk_predictor, template_path, template_scale=2, size_threshold=999):
     align_out_size = (512, 512)
     ref_points = np.load(template_path) / template_scale
-        
+
     # Detect landmark points
     face_dets = face_detector(img, 1)
     assert len(face_dets) > 0, 'No faces detected'
@@ -27,13 +27,13 @@ def detect_and_align_faces(img, face_detector, lmk_predictor, template_path, tem
     tform_params = []
     for det in face_dets:
         if isinstance(face_detector, dlib.cnn_face_detection_model_v1):
-            rec = det.rect # for cnn detector
+            rec = det.rect  # for cnn detector
         else:
             rec = det
-        if rec.width() > size_threshold or rec.height() > size_threshold: 
+        if rec.width() > size_threshold or rec.height() > size_threshold:
             print('Face is too large')
             break
-        landmark_points = lmk_predictor(img, rec) 
+        landmark_points = lmk_predictor(img, rec)
         single_points = []
         for i in range(5):
             single_points.append([landmark_points.part(i).x, landmark_points.part(i).y])
@@ -41,7 +41,7 @@ def detect_and_align_faces(img, face_detector, lmk_predictor, template_path, tem
         tform = trans.SimilarityTransform()
         tform.estimate(single_points, ref_points)
         tmp_face = trans.warp(img, tform.inverse, output_shape=align_out_size, order=3)
-        aligned_faces.append(tmp_face*255)
+        aligned_faces.append(tmp_face * 255)
         tform_params.append(tform)
     return [aligned_faces, tform_params]
 
@@ -71,16 +71,16 @@ def enhance_faces(LQ_faces, model):
 
 def past_faces_back(img, hq_faces, tform_params, upscale=1):
     h, w = img.shape[:2]
-    img = cv2.resize(img, (int(w*upscale), int(h*upscale)), interpolation=cv2.INTER_CUBIC)
+    img = cv2.resize(img, (int(w * upscale), int(h * upscale)), interpolation=cv2.INTER_CUBIC)
     for hq_img, tform in tqdm(zip(hq_faces, tform_params), total=len(hq_faces)):
-        tform.params[0:2,0:2] /= upscale
-        back_img = trans.warp(hq_img/255., tform, output_shape=[int(h*upscale), int(w*upscale)], order=3) * 255
-        
+        tform.params[0:2, 0:2] /= upscale
+        back_img = trans.warp(hq_img / 255., tform, output_shape=[int(h * upscale), int(w * upscale)], order=3) * 255
+
         # blur mask to avoid border artifacts
-        mask = (back_img == 0) 
-        mask = cv2.blur(mask.astype(np.float32), (5,5))
+        mask = (back_img == 0)
+        mask = cv2.blur(mask.astype(np.float32), (5, 5))
         mask = (mask > 0)
-        img = img * mask + (1 - mask) * back_img 
+        img = img * mask + (1 - mask) * back_img
     return img.astype(np.uint8)
 
 
@@ -98,12 +98,13 @@ if __name__ == '__main__':
     template_path = './pretrain_models/FFHQ_template.npy'
 
     print('======> Loading images, crop and align faces.')
-    img_path = opt.test_img_path 
+    img_path = opt.test_img_path
     img = dlib.load_rgb_image(img_path)
+    assert (img.shape == (512, 512, 3), 'Input image size should be 512 * 512 with 3 channels.')
     # aligned_faces, tform_params = detect_and_align_faces(img, face_detector, lmk_predictor, template_path)
     aligned_faces, tform_params = np.array([img]), None
     # Save aligned LQ faces
-    save_lq_dir = os.path.join(opt.results_dir, 'LQ_faces') 
+    save_lq_dir = os.path.join(opt.results_dir, 'LQ_faces')
     os.makedirs(save_lq_dir, exist_ok=True)
     print('======> Saving aligned LQ faces to', save_lq_dir)
     save_imgs(aligned_faces, save_lq_dir)
@@ -111,8 +112,8 @@ if __name__ == '__main__':
     enhance_model = def_models(opt)
     hq_faces, lq_parse_maps = enhance_faces(aligned_faces, enhance_model)
     # Save LQ parsing maps and enhanced faces
-    save_parse_dir = os.path.join(opt.results_dir, 'ParseMaps') 
-    save_hq_dir = os.path.join(opt.results_dir, 'HQ') 
+    save_parse_dir = os.path.join(opt.results_dir, 'ParseMaps')
+    save_hq_dir = os.path.join(opt.results_dir, 'HQ')
     os.makedirs(save_parse_dir, exist_ok=True)
     os.makedirs(save_hq_dir, exist_ok=True)
     print('======> Save parsing map and the enhanced faces.')
@@ -124,5 +125,3 @@ if __name__ == '__main__':
     # final_save_path = os.path.join(opt.results_dir, 'hq_final.jpg')
     # print('======> Save final result to', final_save_path)
     # io.imsave(final_save_path, hq_img)
-
-
